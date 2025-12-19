ac7120f195494d1bc7d6f193b9e812cf4e253804
human-11 <human-11@DESKTOP-OTOM00D>
2025-12-19 11:33:20 +0900

    1111

---
+++ rstr/src/main/java/web/com/rstr/a02_mvc/A03_MVC_Dao.java
@@ -0,0 +1,14 @@
+package web.com.rstr.a02_mvc;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+@Mapper
+public interface A03_MVC_Dao {
+	
+	@Select("SELECT * FROM DEPT")
+	List<Dept> getDeptList();
+}
