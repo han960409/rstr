80a691283d9dc6b47be40d749382ec1d3ec16286
human-11 <human-11@DESKTOP-OTOM00D>
2025-12-19 13:46:32 +0900

    1111

---
+++ rstr/src/main/java/web/com/springweb/a00_prjmvc/RestaurantService.java
@@ -0,0 +1,22 @@
+package web.com.springweb.a00_prjmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import web.com.springweb.a00_prjdto.Restaurant;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private Dao dao;  // ✅ static 제거
+
+    // ✅ 인스턴스 메서드로 변경
+    public String getRestaurant() {
+        return dao.getRestaurant();
+    }
+
+}
+
+	
+
