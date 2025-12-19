24324e65f9680ef100247a41a25617299f392a41
26 <26@DESKTOP-LUOPKD3>
2025-12-19 12:41:39 +0900

    123124

--- rstr/src/main/java/web/com/rstr/Prj04Application.java
+++ rstr/src/main/java/web/com/rstr/Prj04Application.java
@@ -1,5 +1,15 @@
 package web.com.rstr;
 
-public class 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Prj04Application {
+
+   public static void main(String[] args) {
+      SpringApplication.run(Prj04Application.class, args);
+   }
 
 }
+
