7e2e4896a51126c870156c988e6df33375154a92
human-11 <human-11@DESKTOP-OTOM00D>
2025-12-19 12:15:25 +0900

    1111

--- rstr/src/test/java/web/com/prj04/Prj04Application3.java
+++ rstr/src/test/java/web/com/prj04/Prj04Application3.java
@@ -1,4 +1,4 @@
-package web.com.rstr;
+package web.com.prj04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- rstr/src/test/java/web/com/prj04/sdfa.java
+++ rstr/src/test/java/web/com/prj04/sdfa.java
@@ -1,4 +1,4 @@
-package web.com.rstr;
+package web.com.prj04;
 
 public class sdfa {
 
